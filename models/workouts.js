8853eb1b27a14fc3b226d6b0ba73aeb802245abb
Your Name <danygrimaldo@gmail.com>
2020-06-30 13:20:45 -0400

    added viewjs and removed exercises model to only have a workouts model

--- models/workouts.js
+++ models/workouts.js
@@ -2,19 +2,56 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutsSchema = new Schema({
-  name: {
-    type: String,
-    unique: true,
-  },
-  exercises: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise",
+const workoutsSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date(),
     },
-  ],
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "What type of exercise is this?",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "What is the name of your exercise?",
+        },
+        duration: {
+          type: Number,
+          required: "How long did this exercise last? (Enter value in minutes)",
+        },
+        weight: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutsSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
-const Workouts = mongoose.model("Workouts", WorkoutsSchema);
+const Workouts = mongoose.model("Workouts", workoutsSchema);
 
 module.exports = Workouts;
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,23 @@
 //Referring to Act26 from w17 miniProj
 
 const router = require("express").Router();
-const Exercise = require("../models/exercise.js");
 const Workouts = require("../models/workouts.js");
+
+//PUT - "ID and UPDATE"
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workouts.create(body)
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+//DELETE - FIND BY ID and DELETE
+
+//api/workouts/range -
+//".find" + ".limit"
 
 module.exports = router;
